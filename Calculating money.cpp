6a9992c580dd6c07caf1c25da925fbfd04467492
Vaibhavy101 <87309673+Vaibhavy101@users.noreply.github.com>
2021-08-27 18:33:24 +0530

    Add files via upload

---
+++ Calculating money.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n, total, rate=50;
+	cout<<"Enter the no of days worked\n";
+	cin>>n;
+	
+	total= n*50;
+	
+	cout<<"for n days you have earned:\n"<<total;
+	
+	return 0;
+	
+}
---
+++ Unary operator prefix and postfix.cpp
@@ -0,0 +1,13 @@
+//unary operator postfix and prefix
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 4, b = 5;
+	cout<<a++<<endl;
+	cout<<++b<<endl;
+	
+	
+	
+	return 0;
+}
