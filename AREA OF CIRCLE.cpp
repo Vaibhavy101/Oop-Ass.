a37c5243d164951384809b030eed77ad94dad0fc
Vaibhavy101 <87309673+Vaibhavy101@users.noreply.github.com>
2021-08-27 18:17:04 +0530

    Add files via upload

---
+++ AREA OF CIRCLE.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float r,area,circumference;
+	
+	float pi = 3.14, Two=2 ;
+	cout<<"\n Enter the value of r :";
+	cin>>r;
+	area = pi*r*r;
+	cout<<"\n the area of cirlce is :"<<area;
+	circumference = Two*pi*r;
+	cout<<"\n circumference of circle is:"<<circumference;
+	return 0;
+}
+
+
